b05ded244017eed936d05c7ec31668d2a5ad08db
enzogran01 <yournarrator013@gmail.com>
2026-03-28 22:52:57 -0300

    correcao aula17 middleware.js

--- node/aula17/src/middlewares/middleware.js
+++ node/aula17/src/middlewares/middleware.js
@@ -1,12 +1,12 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
     next();
 };
 
-exports.checkCsrfError = (err, req, res) => {
+exports.checkCsrfError = (err, req, res, next) => {
     if(err && 'EBADCSRFTOKEN' === err.code) {
         return res.render('404');
     };
+    next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
